0ea389979c0e79b055b98888d896be795c4c42c6
Kevin Cozart <Cozartkevin@gmail.com>
2023-04-24 13:12:05 -0700

    Code Forest Setup
    Setting up project Code Forest.  Initial commit for Code forest as well as other projects such as GrisClone_Test_V0.02, and PlatFormer projects.  Various project updates to Poorly2.0 after running it on a higher version of unity than the previous time it was run.

--- Personal/LoFiTest/Assets/Scripts/CameraFollow.cs
+++ Personal/LoFiTest/Assets/Scripts/CameraFollow.cs
@@ -13,7 +13,7 @@ public class CameraFollow : MonoBehaviour
         Vector3 desiredPosition = target.position + offset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, (smoothSpeed * Time.deltaTime));
         transform.position = smoothedPosition;
-
+        
         transform.LookAt(target);
     }
 
